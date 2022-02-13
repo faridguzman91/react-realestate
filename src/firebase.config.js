2dfe72af56a608364c4089732c00ca1b3084053c
Jimmy Guzman <93182272+faridwoonmodule@users.noreply.github.com>
2022-02-13 22:42:54 +0100

    added assets, deleted app.css, app.test.js.added index.css

---
+++ src/firebase.config.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore} from 'firebase/firestore'
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDIuVvi97D6vbRFTtznU2GysLm68cmh6F4",
+  authDomain: "react-housemarketplaceapp.firebaseapp.com",
+  projectId: "react-housemarketplaceapp",
+  storageBucket: "react-housemarketplaceapp.appspot.com",
+  messagingSenderId: "975213576532",
+  appId: "1:975213576532:web:808bdac6a80492136e08c1",
+  measurementId: "G-J2S26R9WVE",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore()
